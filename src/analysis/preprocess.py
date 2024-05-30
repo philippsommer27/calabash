6fb76046fcae46bec6b136d608180e23d44c0cfd
Philipp Sommerhalter <philipp.sommerhalter@sig.eu>
2024-05-30 12:37:04 +0200

    preprocess

--- src/analysis/preprocess.py
+++ src/analysis/preprocess.py
@@ -1,16 +1,20 @@
 import json
 
-def correct_json(file_path):
+def read(file_path):
     with open(file_path, 'r') as file:
         content = file.read().strip()
+    return content
+    
+def write(file_path, content):
+    with open(file_path, 'w') as file:
+        file.write(content)
 
+def prune(content):
+    return content[0:content.rindex('{"host"')]
+
+def correct_format(content):
     parts = content.split('}{')
 
     corrected_content = '[' + '},{'.join(parts) + ']'
 
-    with open('out/corrected_output.json', 'w') as file:
-        file.write(corrected_content)
-
     return corrected_content
-
-def 
