c0566d6e96d216fa7954a329a53ea174eb8bbb2b
Philip Sommerhalter <philippsommerhalter@gmail.com>
2024-06-27 13:24:20 +0200

    Analyze multiple runs

--- src/analysis/analysis_runner.py
+++ src/analysis/analysis_runner.py
@@ -4,6 +4,7 @@ from process_ptrace import resolve
 from to_df import ScaphandreToDf
 from misc.config import load_configuration
 from misc.util import read_json, get_display_name, read_file, create_directory, write_json
+import pandas as pd
 
 def run(config_path):
     config = load_configuration(config_path)
@@ -11,6 +12,7 @@ def run(config_path):
     for image in config['images']:
         display_name = get_display_name(image)
         print(f"Running analysis for {display_name}")
+        accumulated = {}
         for i in range(config['procedure']['external_repetitions']):
             print(f"Repetition {i}")
             # Setup
@@ -51,9 +53,21 @@ def run(config_path):
             analysis = Analysis(converter.dfs, config['procedure']['internal_repetitions'])
             analysis.do()
             write_json(f"{directory}/analysis.json", analysis.results)
+            accumulated[i] = analysis.results
+
+        # Analyze Multiple
+        if config['procedure']['external_repetitions'] > 1:
+            df = analyze_multiple(accumulated)
+            df.describe().to_csv(f"{config['out']}/{display_name}/summary.csv")
     
-def analyze_multiple():
-    pass
+def analyze_multiple(data):
+    flattened_data = {outer_k: {f"{inner_k}_{k}": v for inner_k, inner_v in outer_v.items() for k, v in inner_v.items()} for outer_k, outer_v in data.items()}
+
+    df = pd.DataFrame.from_dict(flattened_data, orient='index')
+
+    df.columns = df.columns.str.replace('analysis_', '', regex=False)
+
+    return df
 
 if __name__ == '__main__':
     run("test.yaml")
